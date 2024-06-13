59ebee54034c2b7e9e391258823dd4d997017c49
Mijael30100 <mijaelkoro@gmail.com>
2024-06-13 17:07:33 -0300

    ArticuloDao Lists Implementation

--- ProgramaGestionDeInventario/src/main/java/com/gestiondeproyectos/ProgramaGestionDeInventario/dao/ArticuloDao.java
+++ ProgramaGestionDeInventario/src/main/java/com/gestiondeproyectos/ProgramaGestionDeInventario/dao/ArticuloDao.java
@@ -4,7 +4,52 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticuloDao extends JpaRepository<Articulo, Long>{
+
+    @Query(value = "SELECT * FROM articulo a ORDER BY a.nombre ASC", nativeQuery = true)
+    List<Articulo> listItems();
     
+    @Query(value = "SELECT a.*, c.descripcion AS categoria_descripcion FROM Articulo a " + 
+                "JOIN CategoriaPorArticulo cpa ON a.iden = cpa.articulo_iden " + 
+                "JOIN Categoria c ON cpa.categoria_iden = c.iden " + 
+                "ORDER BY c.iden ASC, a.nombre ASC; ", nativeQuery = true)
+    List<Articulo> listItemsByCategory();
+
+    @Query(value = "SELECT a.*, p.nombre AS nombre_proveedor, p.email AS email_proveedor FROM Articulo a " + 
+                "JOIN CategoriaPorArticulo cpa ON a.iden = cpa.articulo_iden " + 
+                "JOIN Categoria c ON cpa.categoria_iden = c.iden " + 
+                "JOIN categoriaporproveedor cpp ON c.iden = cpp.categoria_iden " +
+                "JOIN proveedor p ON cpp.categoria_iden = p.iden " +
+                "ORDER BY p.iden ASC, a.nombre ASC; ", nativeQuery = true)
+    List<Articulo> listItemsBySupplier();
+
+    @Query("SELECT a FROM Articulo a " +
+           "JOIN CategoriaPorArticulo cpa ON a.iden = cpa.articulo_iden " +
+           "JOIN Categoria c ON cpa.categoria_iden = c.iden " +
+           "WHERE a.descripcion LIKE %:descripcion% " +
+           "ORDER BY a.nombre ASC")
+    List<Articulo> listItemsByCategoryDescription(
+            @Param("descripcion") String descripcion);
+
+    @Query("SELECT a FROM Articulo a " +
+           "JOIN CategoriaPorArticulo cpa ON a.iden = cpa.articulo_iden " +
+           "JOIN CategoriaPorProveedor cpp ON cpa.categoria_iden = cpp.categoria_iden " +
+           "JOIN Proveedor p ON cpp.proveedor_iden = p.iden " +
+           "WHERE p.nombre LIKE %:nombreProveedor% " +
+           "ORDER BY a.nombre ASC")
+    List<Articulo> listItemsBySupplierName(
+            @Param("nombreProveedor") String nombreProveedor);
+    
+    @Query("SELECT a FROM Articulo a " +
+           "WHERE a.nombre LIKE %:nombre% " +
+           "ORDER BY a.nombre ASC")
+    List<Articulo> listItemsByName(@Param("nombre") String nombre);
+
+    @Query("SELECT a FROM Articulo a " +
+           "WHERE a.codigo = :Iden " +
+           "ORDER BY a.nombre ASC")
+    List<Articulo> listItemsByIden(@Param("Iden") String Iden);
+
 }
--- ProgramaGestionDeInventario/src/main/java/com/gestiondeproyectos/ProgramaGestionDeInventario/service/ArticuloServiceImpl.java
+++ ProgramaGestionDeInventario/src/main/java/com/gestiondeproyectos/ProgramaGestionDeInventario/service/ArticuloServiceImpl.java
@@ -2,6 +2,8 @@ package com.gestiondeproyectos.ProgramaGestionDeInventario.service;
 import com.gestiondeproyectos.ProgramaGestionDeInventario.dao.ArticuloDao;
 import com.gestiondeproyectos.ProgramaGestionDeInventario.model.Articulo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +12,33 @@ public class ArticuloServiceImpl implements ArticuloService{
     
     @Autowired
     private ArticuloDao articuloDao;
+
+    public List<Articulo> searchItemsBySupplierName(String nombreProveedor) {
+        return articuloDao.listItemsBySupplierName(nombreProveedor);
+    }
+
+    public List<Articulo> searchItems() {
+        return articuloDao.listItems();
+    }
+    
+    public List<Articulo> searchItemsByCategory() {
+        return articuloDao.listItemsByCategory();
+    }
+
+    public List<Articulo> searchItemsBySupplier() {
+        return articuloDao.listItemsBySupplier();
+    }
+
+    public List<Articulo> searchItemsByCategoryDescription(String descripcion) {
+        return articuloDao.listItemsByCategoryDescription(descripcion);
+    }
+
+    public List<Articulo> searchItemsByName(String nombre) {
+        return articuloDao.listItemsByName(nombre);
+    }
+
+    public List<Articulo> searchItemsByCode(String codigo) {
+        return articuloDao.listItemsByIden(codigo);
+    }
 
 }
