66debc99c02ce93feefa1f1840f6a9207f2234b8
alanisF <alanisfacundo@hotmail.com>
2024-06-23 16:11:09 -0300

    Incorporación de Categorias en el ProveedorController

--- ProgramaGestionDeInventario/src/main/java/com/gestiondeproyectos/ProgramaGestionDeInventario/controller/ProveedorController.java
+++ ProgramaGestionDeInventario/src/main/java/com/gestiondeproyectos/ProgramaGestionDeInventario/controller/ProveedorController.java
@@ -1,5 +1,6 @@
 package com.gestiondeproyectos.ProgramaGestionDeInventario.controller;
 
+import com.gestiondeproyectos.ProgramaGestionDeInventario.service.CategoriaService;
 import com.gestiondeproyectos.ProgramaGestionDeInventario.service.PersonaService;
 import com.gestiondeproyectos.ProgramaGestionDeInventario.service.ProveedorService;
 import com.gestiondeproyectos.ProgramaGestionDeInventario.model.Proveedor;
@@ -18,30 +19,38 @@ public class ProveedorController {
 
     private PersonaService personaService;
 
+    private CategoriaService categoriaService;
+
     @Autowired
     public ProveedorController(ProveedorService proveedorService) {
         this.proveedorService = proveedorService;
     }
 
        @GetMapping("/agregarProveedor")
-    public String agregarProveedor(Proveedor proveedor, Model model){
+    public String agregarProveedor(Proveedor proveedor, Model modelPersonas, Model modelCategorias){
         var personas = personaService.listarPersonas();
-        model.addAttribute("personas", personas);
+        var categorias = categoriaService.listarCategorias();
+        modelPersonas.addAttribute("personas", personas);
+        modelCategorias.addAttribute("categorias", categorias);
         return "modificarProveedor";
     }
     
     @PostMapping("/guardarProveedor")
     public String guardarProveedor(Proveedor proveedor, Model model){
         var personas = personaService.listarPersonas();
+        var categorias = categoriaService.listarCategorias();
         model.addAttribute("personas", personas);
+        model.addAttribute("categorias", categorias);
         proveedorService.guardar(proveedor);
         return "redirect:/";
     }
 
     @GetMapping("/editarProveedor/{iden}")
-    public String editarProveedor(Proveedor proveedor, Model modelProveedor, Model modelPersona) {
+    public String editarProveedor(Proveedor proveedor, Model modelProveedor, Model modelPersona, Model modelCategoria) {
         var personas = personaService.listarPersonas();
         modelPersona.addAttribute("personas", personas);
+        var categorias = categoriaService.listarCategorias();
+        modelCategoria.addAttribute("categorias", categorias);
         proveedor = proveedorService.encontrarProveedor(proveedor);
         modelProveedor.addAttribute("proveedor", proveedor);
         return "modificarProveedor";
