37d647e4b083f375e02676834ca0bbba7d2f9437
juanf <juanfranza90@gmail.com>
2024-07-06 11:43:09 -0300

    Agregando restricciones para el creado de usuarios con mismo nombre o mail

--- ProgramaGestionDeInventario/src/main/java/com/gestiondeproyectos/ProgramaGestionDeInventario/service/UsuarioService.java
+++ ProgramaGestionDeInventario/src/main/java/com/gestiondeproyectos/ProgramaGestionDeInventario/service/UsuarioService.java
@@ -10,7 +10,7 @@ public interface UsuarioService {
 
     //void saveAdmin(Usuario user);
     //void saveRegularUser(Usuario user);
-    Optional<Usuario> findByUsername(String username);
+    Usuario findByUsername(String username);
     void guardar(Usuario user);
     public List<Usuario> listarUsuarios();
     public Usuario findByEmail(String email);
